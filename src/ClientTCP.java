ad12ca5cd34f29e4cf9c92fba164e6dcdfc60e8f
tariki <aabouchazougarthtarik@elpuig.xeill.net>
2023-03-10 17:20:45 +0100

    .159

--- src/ClientTCP.java
+++ src/ClientTCP.java
@@ -3,27 +3,42 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class ClientTCP {
     private static final String SERVER_ADDRESS = "localhost";
     private static final int SERVER_PORT = 5556;
+    private static Scanner scanner = new Scanner(System.in);
+    private static boolean infinite = true;
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        Llista llista = new Llista("probando", Arrays.asList(1, 2, 3, 2, 1, 4, 5));
-        System.out.println("Enviant els numeros al server: " + llista.getNom() + " " + llista.getNumberList());
+        while (infinite){
+            System.out.println("Posa 7 numeros separats per espais en blancs");
+            int n1 = scanner.nextInt();
+            int n2 = scanner.nextInt();
+            int n3 = scanner.nextInt();
+            int n4 = scanner.nextInt();
+            int n5 = scanner.nextInt();
+            int n6 = scanner.nextInt();
+            int n7 = scanner.nextInt();
 
-        Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
-        ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
-        ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
+            Llista llista = new Llista("probando", Arrays.asList(n1, n2, n3, n4, n5, n6, n7));
+            System.out.println("Enviant els numeros al server: " + llista.getNom() + " " + llista.getNumberList());
 
-        outputStream.writeObject(llista);
-        outputStream.flush();
+            Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+            ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
+            ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
 
-        Llista result = (Llista) inputStream.readObject();
-        System.out.println("El servidor ha terminat d'ordenar i esborrar: " + result.getNom() + " " + result.getNumberList());
+            outputStream.writeObject(llista);
+            outputStream.flush();
 
-        inputStream.close();
-        outputStream.close();
-        socket.close();
+            Llista result = (Llista) inputStream.readObject();
+            System.out.println("El servidor ha terminat d'ordenar i esborrar: " + result.getNom() + " " + result.getNumberList());
+
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+        }
+
     }
 }
--- src/ThreadServer.java
+++ src/ThreadServer.java
@@ -8,6 +8,7 @@ import java.util.stream.Collectors;
 public class ThreadServer implements Runnable {
     /* Thread que gestiona la comunicació de ServerTCP.java i un client ClientTCP.java */
     private Socket clientSocket;
+    private boolean infinite = true;
 
     public ThreadServer(Socket clientSocket) {
         this.clientSocket = clientSocket;
@@ -15,23 +16,25 @@ public class ThreadServer implements Runnable {
 
     @Override
     public void run() {
-        try {
-            ObjectInputStream inputStream = new ObjectInputStream(clientSocket.getInputStream());
-            ObjectOutputStream outputStream = new ObjectOutputStream(clientSocket.getOutputStream());
+        while (infinite){
+            try {
+                ObjectInputStream inputStream = new ObjectInputStream(clientSocket.getInputStream());
+                ObjectOutputStream outputStream = new ObjectOutputStream(clientSocket.getOutputStream());
 
-            Llista llista = (Llista) inputStream.readObject();
-            System.out.println("Received object from client: " + llista.getNom() + " " + llista.getNumberList());
+                Llista llista = (Llista) inputStream.readObject();
+                System.out.println("Received object from client: " + llista.getNom() + " " + llista.getNumberList());
 
-            List<Integer> uniqueNumbers = llista.getNumberList().stream().distinct().sorted().collect(Collectors.toList());
-            Llista result = new Llista(llista.getNom(), uniqueNumbers);
-            outputStream.writeObject(result);
+                List<Integer> uniqueNumbers = llista.getNumberList().stream().distinct().sorted().collect(Collectors.toList());
+                Llista result = new Llista(llista.getNom(), uniqueNumbers);
+                outputStream.writeObject(result);
 
-            inputStream.close();
-            outputStream.close();
-            clientSocket.close();
+                inputStream.close();
+                outputStream.close();
+                clientSocket.close();
 
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
